2b3d9107d36fefa780d55c049db7094d4c9a0d4a
dhrutip <dhruti.pandya@gmail.com>
2017-02-14 19:59:00 -0800

    WinchSpeedPID

---
+++ src/Commands/WinchSpeedPID.cpp
@@ -0,0 +1,52 @@
+#include "WinchSpeedPID.h"
+
+WinchSpeedPID::WinchSpeedPID() {
+	// Use Requires() here to declare subsystem dependencies
+	// eg. Requires(Robot::chassis.get());
+	Requires(Winch);
+	 _lastButton1 = false;
+	 _talon->SetControlMode(CANSpeedController::kPercentVbus);
+	 _talon->ConfigLimitMode(CANSpeedController::kLimitMode_SrxDisableSwitchInputs);
+	 correctionValue = 0.5;
+}
+
+// Called just before this Command runs the first time
+void WinchSpeedPID::Initialize()
+{
+	_talon->SetF(0.0); // TODO: Set F constant through testing
+	_talon->SetP(0.1); // TODO: Set P constant through testing
+	_talon->SetI(0.0); // TODO: Set I constant through testing
+	_talon->SetD(0.0); // TODO: Set D constant through testing
+	 // TODO: set correctionValue through testing
+	setPoint = (_talon->GetPosition() - correctionValue);
+
+}
+
+// Called repeatedly when this Command is scheduled to run
+void WinchSpeedPID::Execute()
+{
+	currentPoint = _talon->GetPosition();
+	distFromSetPoint = (setPoint - currentPoint);
+	targetPositionRotations = (distFromSetPoint / 5.655);
+	_talon->Set(targetPositionRotations);
+}
+
+// Make this return true when this Command no longer needs to run execute()
+bool WinchSpeedPID::IsFinished()
+{
+	if(_lastButton1 == true)
+	{
+		return true;
+	}
+	return false;
+}
+
+void WinchSpeedPID::End()
+{
+
+}
+
+void WinchSpeedPID::Interrupted()
+{
+
+}
---
+++ src/Commands/WinchSpeedPID.h
@@ -0,0 +1,29 @@
+#ifndef WinchSpeedPID_H
+#define WinchSpeedPID_H
+
+#include "../CommandBase.h"
+#include "../Subsystems/Winch.h"
+using namespace frc;
+
+class WinchSpeedPID : public CommandBase {
+
+private:
+	CANTalon* _talon;
+	bool _lastButton1;
+	double targetPositionRotations;
+	double currentPoint;
+	double setPoint;
+	double correctionValue;
+	double moveValue;
+	double distFromSetPoint;
+
+public:
+	WinchSpeedPID();
+	void Initialize();
+	void Execute();
+	bool IsFinished();
+	void End();
+	void Interrupted();
+};
+
+#endif  // WinchSpeedPID_H
